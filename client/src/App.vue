39e2e3941db0c36eeba0aa0408e2c7d95b8a86bd
peterhubina <peter.hubina1@gmail.com>
2024-11-09 08:13:08 +0000

    join new channel fix

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,6 +3,11 @@
 </template>
 
 <script setup lang="ts">
+import { useChannelStore } from 'stores/channel';
+
+const channelStore = useChannelStore();
+channelStore.fetchChannels();
+
 defineOptions({
   name: 'App'
 });
